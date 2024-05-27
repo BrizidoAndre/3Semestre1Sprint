84f9007064547afbbe3d3b9072fe941179f0f27f
André Brizido Basílio <abrizidobasilio@gmail.com>
2024-05-27 08:53:33 -0300

    uploading senaify

---
+++ datePickerExemplo/app/(tabs)/index.tsx
@@ -0,0 +1,76 @@
+import { Image, StyleSheet, Platform, Text, Button } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { useState } from 'react';
+import DateTimePicker from 'react-native-modal-datetime-picker';
+import DatePicker from 'react-native-date-picker';
+
+export default function HomeScreen() {
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date: Date) => {
+    console.warn("A date has been picked: ", date);
+    hideDatePicker();
+  };
+
+  const testing = (date: Date) => {
+    console.log(date)
+  }
+
+
+
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <Text>Testando</Text>
+
+        <Button title="Show Date Picker" onPress={showDatePicker} />
+        <DateTimePicker
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+          onChange={testing}
+        />
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
